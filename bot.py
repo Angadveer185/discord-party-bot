7ef176924904ced125433771af7adb8faf88062d
Angadveer Singh Chhoker <angad185singh@gmail.com>
2026-05-28 21:52:03 +0530

    Updated.

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
 import discord
 from discord.ext import commands
 import os
+from dotenv import load_dotenv
 
-TOKEN = os.getenv("DISCORD_TOKEN")
+load_dotenv()
+
+TOKEN = os.getenv("BOT_TOKEN")
 
 intents = discord.Intents.default()
 intents.voice_states = True
@@ -11,9 +14,9 @@ intents.members = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 PARTY_CHANNELS = {
-    "party-1-🎧",
-    "party-2-🎧",
-    "party-3-🎧"
+    1479870047311761428,
+    937927167994630208,
+    937927788973944842
 }
 
 @bot.event
@@ -22,12 +25,14 @@ async def on_ready():
 
 @bot.event
 async def on_voice_state_update(member, before, after):
+    if member.bot:
+        return
 
     if after.channel is not None:
 
         channel = after.channel
 
-        if channel.name in PARTY_CHANNELS:
+        if channel.id in PARTY_CHANNELS:
 
             if len(channel.members) == 1:
 
@@ -37,10 +42,14 @@ async def on_voice_state_update(member, before, after):
                 )
 
                 if announcements:
-                    await announcements.send(
-                        f"@everyone 🎮 "
-                        f"**{member.display_name}** started a party in "
-                        f"**{channel.name}**!"
+                    embed = discord.Embed(
+                        title="🎮 Party Started!",
+                        description=(
+                            f"**{member.display_name}** has started a party in "
+                            f"**{channel.name}**"
+                        ),
+                        color=discord.Color.blue()
                     )
+                    await announcements.send("@everyone", embed=embed)
 
 bot.run(TOKEN)
