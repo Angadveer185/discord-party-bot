9fe4688851f6f31ab65e523380bfe808821bfb48
Angadveer Singh Chhoker <angad185singh@gmail.com>
2026-06-15 00:31:19 +0530

    Fixed some issues. Added a cooldown.

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import os
+import time
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -19,37 +20,68 @@ PARTY_CHANNELS = {
     937927788973944842
 }
 
+COOLDOWN = 10  # seconds
+
+active_parties = set()
+channel_cooldowns = {}
+
+
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
+
 
 @bot.event
 async def on_voice_state_update(member, before, after):
     if member.bot:
         return
 
-    if after.channel is not None:
+    if before.channel == after.channel:
+        return
+
+    if after.channel and after.channel.id in PARTY_CHANNELS:
 
         channel = after.channel
 
-        if channel.id in PARTY_CHANNELS:
+        members_before = len(channel.members) - 1
 
-            if len(channel.members) == 1:
+        if members_before == 0 and channel.id not in active_parties:
 
-                announcements = discord.utils.get(
-                    member.guild.text_channels,
-                    name="announcements"
+            now = time.time()
+
+            if (
+                channel.id in channel_cooldowns and
+                now - channel_cooldowns[channel.id] < COOLDOWN
+            ):
+                return
+
+            active_parties.add(channel.id)
+            channel_cooldowns[channel.id] = now
+
+            announcements = discord.utils.get(
+                member.guild.text_channels,
+                name="announcements"
+            )
+
+            if announcements:
+                embed = discord.Embed(
+                    title="🎮 Party Started!",
+                    description=(
+                        f"**{member.display_name}** has started a party in "
+                        f"**{channel.name}**"
+                    ),
+                    color=discord.Color.blue()
                 )
 
-                if announcements:
-                    embed = discord.Embed(
-                        title="🎮 Party Started!",
-                        description=(
-                            f"**{member.display_name}** has started a party in "
-                            f"**{channel.name}**"
-                        ),
-                        color=discord.Color.blue()
-                    )
-                    await announcements.send("@everyone", embed=embed)
+                await announcements.send(
+                    "@everyone",
+                    embed=embed
+                )
+
+    if before.channel and before.channel.id in PARTY_CHANNELS:
+
+        if len(before.channel.members) == 0:
+            active_parties.discard(before.channel.id)
+
 
 bot.run(TOKEN)
